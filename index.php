33674e9a7993bdbfe076af325902cd7d371c819b
flyingwenku <wufei1988@163.com>
2014-01-01 17:12:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 //单行注释
 echo "hello world!";
+echo "<BR>";
 $b = "this is a string";
 echo $b;
+echo "<BR>";
+$a = 5;
+echo $a;
+echo "<BR>";
+echo $a . $b;
+echo "<BR>";
+$a.=$b;
+echo "<BR>";
+echo $a;
 /*
  * 多行注释
 */
