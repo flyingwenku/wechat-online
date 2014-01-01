6aeb7660e39159d82fc0de00be1e0ce7724b9570
flyingwenku <wufei1988@163.com>
2014-01-01 17:58:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ var_dump($arr);
 var_export($arr,true);如果是false结果同var_dump
 
 */
+/*
 $animal_name = array("dog","cat","sleep");
 echo $animal_name[0].$animal_name[1].$animal_name[2]."<BR>";
 $animal_voice = array(
@@ -19,4 +20,28 @@ echo "<BR>";
 var_export($animal_name);
 echo "<BR>";
 var_export($animal_name,true);
+*/
+define("TOKEN", "fred_wechat_test");
+//
+$signature = $_GET['signature'];
+$nonce = $_GET['nonce'];
+$timestamp = $_GET['timestamp'];
+$echostr = $_GET['echostr'];
+
+//
+$tmpArr = array($nonce, TOKEN);
+sort($tmpArr);
+
+//
+$tmpStr = implode($tmpArr);
+
+//
+$tmpStr = shal($tmpStr);
+
+//
+if($tmpStr == $signature) {
+  //
+  echo $echostr;
+}
+
 ?>
