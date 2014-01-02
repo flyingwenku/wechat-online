4cdbf7530cdde556e4ca004dfa7590f58cd0b338
flyingwenku <wufei1988@163.com>
2014-01-02 22:43:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,12 +30,41 @@ class wechat {
 		</xml>";
 
 		$msgType = "text"; //消息类型
-		$contentStr = '我是管理员，请问有什么可以帮到您吗？'; //返回消息内容
+		$contentStr = SimSimi($keyword);//'我是管理员，请问有什么可以帮到您吗？'; //返回消息内容
 
 		//格式化消息模板
 		$resultStr = sprintf($textTpl,$fromUsername,$toUsername,
 		$time,$msgType,$contentStr);
 		echo $resultStr; //输出结果
+	}
+}
+function SimSimi($keyword) {
+
+	//----------- 获取COOKIE ----------//
+	$url = "http://www.simsimi.com/";
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HEADER,1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	$content = curl_exec($ch);
+	list($header, $body) = explode("\r\n\r\n", $content);
+	preg_match("/set\-cookie:([^\r\n]*);/iU", $header, $matches);
+	$cookie = $matches[1];
+	curl_close($ch);
+
+	//----------- 抓 取 回 复 ----------//
+	$url = "http://www.simsimi.com/func/req?lc=ch&msg=$keyword";
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_REFERER, "http://www.simsimi.com/talk.htm?lc=ch");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+	$content = json_decode(curl_exec($ch),1);
+	curl_close($ch);
+
+	if($content['result']=='100') {
+		$content['response'];
+		return $content['response'];
+	} else {
+		return '我还不会回答这个问题...';
 	}
 }
 ?>
