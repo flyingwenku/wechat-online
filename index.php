8214821ed35e080b0a1878ac353e110741a9fb0b
fredwu <wufei1988@163.com>
2014-01-14 22:21:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/**
-  * 微信公众平台
-  *@
-  */
- 
-
 $wechatObj = new wechat();
 $wechatObj->responseMsg();
 class wechat {
@@ -21,7 +15,7 @@ class wechat {
 		$toUsername = $postObj->ToUserName; //获取接收方账号
 		$keyword = trim($postObj->Content); //获取消息内容
 		$time = time(); //获取当前时间戳
-		
+
 
 		//---------- 返 回 数 据 ---------- //
 
@@ -36,16 +30,8 @@ class wechat {
 		</xml>";
 
 		$msgType = "text"; //消息类型
-		//if('text' != $postObj->msgType)
-		//{
-		//	$contentStr = "只支持文本输入！"
-		//}
-		//else
-		//{
-			$contentStr = "这是飞飞飞服务助手的后台服务程序！"//HandleUserInput($keyword);//
-	//	}
-		//sendmail("812135831@qq.com", "飞飞飞服务助手转发", $contentStr)；
-		
+		$contentStr = Weather($keyword);//'我是管理员，请问有什么可以帮到您吗？'; //返回消息内容
+
 		//格式化消息模板
 		$resultStr = sprintf($textTpl,$fromUsername,$toUsername,
 		$time,$msgType,$contentStr);
@@ -53,26 +39,7 @@ class wechat {
 	}
 }
 
-
-function HandleUserInput($keyword) {
-	return "Fred wu".GetSinaWeather($keyword);
-}
-
 /*
-获取天气子函数
-*/
-function GetSinaWeather($keyword){
-	$url = "http://api2.sinaapp.com/search/weather/?appkey=0020130430&appsecert=fa6095e113cd28fd&reqtype=text&keyword=".urlencode($keyword);
-	$weatherJson = file_get_contents($url);
-	$weather = json_decode($weatherJson, true);
-	if($weather['text']['content']){
-	    return $weather['text']['content'];
-	}else{
-	    return "不存在该地点的天气，你的输入有误！";
-	}
-}
- 
- /*
 获取自动机器人回复函数
 */
 function SimSimi($keyword) {
@@ -104,127 +71,18 @@ function SimSimi($keyword) {
 		return '我还不会回答这个问题...';
 	}
 }
-
-$bfconfig = Array (
-	'sitename' => '飞飞飞服务助手',
-	);
-
-$mail = Array (
-	'state' => 1,
-	'server' => 'mail.huayuwireless.com',
-	'port' => 25,
-	'auth' => 1,
-	'username' => 'FredWu',
-	'password' => 'hojywff',
-	'charset' => 'gbk',
-	'mailfrom' => 'wuff@huayuwireless.com'
-	);
-
-function sendmail($mail_to, $mail_subject, $mail_message) {
-
-	global $mail, $bfconfig;
-
-	date_default_timezone_set('PRC');
-
-	$mail_subject = '=?'.$mail['charset'].'?B?'.base64_encode($mail_subject).'?=';
-	$mail_message = chunk_split(base64_encode(preg_replace("/(^|(\r\n))(\.)/", "\1.\3", $mail_message)));
-
-	$headers .= "";
-	$headers .= "MIME-Version:1.0\r\n";
-	$headers .= "Content-type:text/html\r\n";
-	$headers .= "Content-Transfer-Encoding: base64\r\n";
-	$headers .= "From: ".$bfconfig['sitename']."<".$mail['mailfrom'].">\r\n";
-	$headers .= "Date: ".date("r")."\r\n";
-	list($msec, $sec) = explode(" ", microtime());
-	$headers .= "Message-ID: <".date("YmdHis", $sec).".".($msec * 1000000).".".$mail['mailfrom'].">\r\n";
-
-	if(!$fp = fsockopen($mail['server'], $mail['port'], $errno, $errstr, 30)) {
-		exit("CONNECT - Unable to connect to the SMTP server");
+/*
+获取天气子函数
+*/
+function Weather($keyword){
+	$url = "http://api2.sinaapp.com/search/weather/?appkey=0020130430&appsecert=fa6095e113cd28fd&reqtype=text&keyword=".urlencode($keyword);
+	$weatherJson = file_get_contents($url);
+	$weather = json_decode($weatherJson, true);
+	if($weather['text']['content']){
+	    return $weather['text']['content'];
+	}else{
+	    return "不存在该地点的天气，你的输入有误！";
 	}
-
-	stream_set_blocking($fp, true);
-
-	$lastmessage = fgets($fp, 512);
-	if(substr($lastmessage, 0, 3) != '220') {
-		exit("CONNECT - ".$lastmessage);
-	}
-
-	fputs($fp, ($mail['auth'] ? 'EHLO' : 'HELO')." befen\r\n");
-	$lastmessage = fgets($fp, 512);
-	if(substr($lastmessage, 0, 3) != 220 && substr($lastmessage, 0, 3) != 250) {
-		exit("HELO/EHLO - ".$lastmessage);
-	}
-
-	while(1) {
-		if(substr($lastmessage, 3, 1) != '-' || empty($lastmessage)) {
- 			break;
- 		}
- 		$lastmessage = fgets($fp, 512);
-	}
-
-	if($mail['auth']) {
-		fputs($fp, "AUTH LOGIN\r\n");
-		$lastmessage = fgets($fp, 512);
-		if(substr($lastmessage, 0, 3) != 334) {
-			exit($lastmessage);
-		}
-
-		fputs($fp, base64_encode($mail['username'])."\r\n");
-		$lastmessage = fgets($fp, 512);
-		if(substr($lastmessage, 0, 3) != 334) {
-			exit("AUTH LOGIN - ".$lastmessage);
-		}
-
-		fputs($fp, base64_encode($mail['password'])."\r\n");
-		$lastmessage = fgets($fp, 512);
-		if(substr($lastmessage, 0, 3) != 235) {
-			exit("AUTH LOGIN - ".$lastmessage);
-		}
-
-		$email_from = $mail['mailfrom'];
-	}
-
-	fputs($fp, "MAIL FROM: <".preg_replace("/.*\<(.+?)\>.*/", "\\1", $email_from).">\r\n");
-	$lastmessage = fgets($fp, 512);
-	if(substr($lastmessage, 0, 3) != 250) {
-		fputs($fp, "MAIL FROM: <".preg_replace("/.*\<(.+?)\>.*/", "\\1", $email_from).">\r\n");
-		$lastmessage = fgets($fp, 512);
-		if(substr($lastmessage, 0, 3) != 250) {
-			exit("MAIL FROM - ".$lastmessage);
-		}
-	}
-
-	foreach(explode(',', $mail_to) as $touser) {
-		$touser = trim($touser);
-		if($touser) {
-			fputs($fp, "RCPT TO: <".preg_replace("/.*\<(.+?)\>.*/", "\\1", $touser).">\r\n");
-			$lastmessage = fgets($fp, 512);
-			if(substr($lastmessage, 0, 3) != 250) {
-				fputs($fp, "RCPT TO: <".preg_replace("/.*\<(.+?)\>.*/", "\\1", $touser).">\r\n");
-				$lastmessage = fgets($fp, 512);
-				exit("RCPT TO - ".$lastmessage);
-			}
-		}
-	}
-
-	fputs($fp, "DATA\r\n");
-	$lastmessage = fgets($fp, 512);
-	if(substr($lastmessage, 0, 3) != 354) {
-		exit("DATA - ".$lastmessage);
-	}
-
-	fputs($fp, $headers);
-	fputs($fp, "To: ".$mail_to."\r\n");
-	fputs($fp, "Subject: $mail_subject\r\n");
-	fputs($fp, "\r\n\r\n");
-	fputs($fp, "$mail_message\r\n.\r\n");
-	$lastmessage = fgets($fp, 512);
-	if(substr($lastmessage, 0, 3) != 250) {
-		exit("END - ".$lastmessage);
-	}
-
-	fputs($fp, "QUIT\r\n");
-
 }
 
 ?>
