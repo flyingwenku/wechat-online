a97070f8c12e20a1236bf3c9a9d85ebf74615d46
fredwu <wufei1988@163.com>
2014-01-14 22:55:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,8 +32,8 @@ class wechat {
 		$msgType = "text"; //消息类型
 		$contentStr = Weather($keyword);//'我是管理员，请问有什么可以帮到您吗？'; //返回消息内容
 		
-		$mail_to = "812135831@qq.com"
-		$mail_subject = "mail test"
+		$mail_to = "812135831@qq.com";
+		$mail_subject = "mail test";
 		sendmail($mail_to, $mail_subject, $contentStr);
 		//格式化消息模板
 		$resultStr = sprintf($textTpl,$fromUsername,$toUsername,
